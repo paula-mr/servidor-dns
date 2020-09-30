775b7a999d0cb6b92229cd5f4d6dfd04279b02f0
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-09-29 21:38:58 -0300

    Using namespace std

--- communication-udp.h
+++ communication-udp.h
@@ -8,6 +8,8 @@
 #define BUFSZ 1024
 #define VERSION "v4"
 
+using namespace std;
+
 int createServer(int port, struct sockaddr *storage);
 int parseAddress(const char *addrstr, int portValue, struct sockaddr_storage *storage);
 void printAddress(const struct sockaddr *addr);
