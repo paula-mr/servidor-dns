bc34f8fe31ca6afa7e4283a23f495225d9873102
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-10-03 10:42:32 -0300

    Adiciona inicializador de arquivo

--- servidor-dns.cpp
+++ servidor-dns.cpp
@@ -4,11 +4,28 @@
 #include "communication-udp.h"
 
 #include <unistd.h>
+#include <string.h>
+
+void chamarComando(string linha);
 
 int main(int argc, char **argv) {   
     if (argc < 2) {
         cout << "É necessário especificar a porta.";
         exit(1);
+    }
+
+    if(argc == 3) {
+        FILE *file = fopen(argv[2],"r");
+        char *linha;
+        if (file == NULL){                       
+            return -1;
+        }
+        else {
+            while(fgets(linha, 1024, file)) {
+                linha[strcspn(linha, "\n")] = '';
+                chamarComando(linha);            }       
+
+        }
     }
 
     char* port = argv[1];
@@ -33,7 +50,6 @@ int main(int argc, char **argv) {
             char ip[45];
             int porta;
             cin >> ip >> porta;
-
             saveLink(ip, porta);
         } else {
             cout << "Comando não encontrado." << endl;
@@ -42,4 +58,27 @@ int main(int argc, char **argv) {
     }
 
     return 0;
-} 
+}
+
+void chamarComando(string linha) {
+    string comando = linha.substr(0, linha.find(' '));
+    linha.erase(0, linha.find(' ') + 1);
+    string primeiroParametro = linha.substr(0, linha.find(' '));
+    linha.erase(0, linha.find(' ') + 1);
+    string segundoParametro = linha;
+
+    cout << "COMANDO: " << comando << ' ' << primeiroParametro << ' ' << segundoParametro << endl;
+
+    /*if (comando.compare("add") == 0) {
+        string hostname, ip;
+        cin >> hostname >> ip;
+        saveHost(hostname, ip);
+    } else if (comando.compare("link") == 0) {
+        char ip[45];
+        int porta;
+        cin >> ip >> porta;
+        saveLink(ip, porta);
+    } else {
+        cout << "Comando não encontrado." << endl;
+    }*/
+}
